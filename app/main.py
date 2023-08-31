a6b0f97bd37d6850b294c6fb7a79bb95a5dcab98
Sebastian Blum <sebastian.blum@alexanderthamm.com>
2023-08-31 17:19:03 +0200

    added caching

--- app/main.py
+++ app/main.py
@@ -1,18 +1,19 @@
 import pandas as pd
 import streamlit as st
 
-from utils import AWSSession
+from utils import AWSSession, timeit
 
-st.header("AWS Sagemaker Endpoints")
-# st.subheader("Skin Cancer Detection")
+# stores cache for 3600 seconds / 1h
+@st.cache_data(ttl=3600)
+def get_endpoint_response()->list:
+    aws_session = AWSSession()
+    aws_session.set_sessions()
+    response = aws_session.get_sagemaker_endpoints()
+    return response.get("Endpoints")
 
-aws_session = AWSSession()
-aws_session.set_sessions()
-
-response = aws_session.get_sagemaker_endpoints()
-endpoints = response.get("Endpoints")
-
-if endpoints:
+@st.cache_data(ttl=3600)
+@timeit
+def transform_endpoints(endpoints:list) -> pd.DataFrame:
     df = pd.DataFrame.from_records(endpoints)
     df_table = df.drop(columns=["CreationTime", "LastModifiedTime"])
     df_table["Creation Time"] = pd.to_datetime(df.LastModifiedTime).dt.strftime(
@@ -25,34 +26,21 @@ if endpoints:
             "EndpointStatus": "Endpoint Status",
         }
     )
-    st.table(df_table)
+    return df_table
 
+
+
+st.header("AWS Sagemaker Endpoints")
+
+if st.button("Reload endpoints"):
+    # Clear values from *all* all in-memory and on-disk data caches:
+    # i.e. clear values from both square and cube
+    st.cache_data.clear()
+
+endpoints = get_endpoint_response()
+
+if endpoints:
+    table = transform_endpoints(endpoints=endpoints)
+    st.table(table)
 else:
     st.info("There are currently no endpoints deployed", icon="ℹ️")
-
-
-# if test_image:
-#     image = Image.open(test_image)
-#     image_file = io.BytesIO(test_image.getvalue())
-#     files = {"file": image_file}
-
-#     col1, col2 = st.columns(2)
-
-#     with col1:
-#         # Display the uploaded image in the first column
-#         st.image(test_image, caption="", use_column_width="always")
-
-#     with col2:
-#         if st.button("Start Prediction"):
-#             with st.spinner("Prediction in Progress. Please Wait..."):
-#                 # Send a POST request to FastAPI for prediction
-#                 output = requests.post(FASTAPI_ENDPOINT, files=files, timeout=8000)
-#             st.success("Success! Click the Download button below to retrieve prediction results (JSON format)")
-#             # Display the prediction results in JSON format
-#             st.json(output.json())
-#             # Add a download button to download the prediction results as a JSON file
-#             st.download_button(
-#                 label="Download",
-#                 data=json.dumps(output.json()),
-#                 file_name="cnn_skin_cancer_prediction_results.json",
-#             )
